b95550602ccd26c02362d4d206e8098075686390
grbll@posteo.com <grbll@posteo.com>
2025-05-23 14:14:02 +0200

    added example colors and users into table

--- init/01_create_table.sql
+++ init/01_create_table.sql
@@ -7,9 +7,19 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS colors (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(50) NOT NULL
 );
 
 ALTER TABLE users
   ADD COLUMN favorite_color INT,
   ADD FOREIGN KEY (favorite_color) REFERENCES colors(id);
+
+INSERT INTO colors (
+  name
+) VALUES ( "blue" ), ( "red" ), ( "green" )
+
+INSERT INTO users (
+  name,
+  flag,
+  favorite_color
+) VALUES ( "hans", TRUE, 2 ), ("susi", FALSE, 2), ("ben", FALSE, "1")
